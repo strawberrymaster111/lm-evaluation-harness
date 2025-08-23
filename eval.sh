b6aa9610e92b1e12e83ed48130368fd1bd4957e4
strawberrymaster111 <1579166553@qq.com>
2025-08-23 13:42:26 +0000

    0823

--- eval.sh
+++ eval.sh
@@ -22,11 +22,14 @@ filename_list=(
 
 for file_name in "${filename_list[@]}"; 
 do
-    lm_eval --model hf \
-        --model_args "pretrained=/mnt/${file_name}/ckpt-globalstep48000/,tokenizer=/mnt/blob-openpai-xiaoliuinterns-out/hongyi_he/Llama-3-8B-tokenizer" \
-        --tasks commonsense_qa,piqa,mmlu \
-        --device all \
-        --output_path /mnt/${file_name}/ckpt-globalstep48000/eval_results_append_remote.json \
-        --trust_remote_code \
+    for step in $(seq 1000 1000 48000)
+    do
+        cp config.json /mnt/${file_name}/ckpt-globalstep${step}/
+        lm_eval --model hf \
+            --model_args "pretrained=/mnt/${file_name}/ckpt-globalstep${step}/,tokenizer=/mnt/blob-openpai-xiaoliuinterns-out/hongyi_he/Llama-3-8B-tokenizer" \
+            --tasks sciq,winogrande,race,openbookqa,arc_easy,arc_challenge,hellaswag,social_iqa,winogrande,race,commonsense_qa,piqa,mmlu \
+            --device all \
+            --output_path /mnt/${file_name}/ckpt-globalstep${step}/eval_results_full_remote.json \
+            --trust_remote_code \
 
 done
