fcc2f3b15f7f63aa27f6f85d457634303ed2f620
lzh0525 <1341152362@qq.com>
2026-03-25 22:10:40 +0000

    update

--- eval_1.5B.sh
+++ eval_1.5B.sh
@@ -21,7 +21,7 @@ do
     do
         cp config.json /mnt/blob-openpai-xiaoliuinterns-out/${file_name}/ckpt-globalstep${step}/
         lm_eval --model hf \
-            --model_args "pretrained=/mnt/blob-openpai-xiaoliuinterns-out/${file_name}/ckpt-globalstep${step}/,tokenizer=/mnt/blob-openpai-xiaoliuinterns-out/openpai/hongyi_he/Llama-3-8B-tokenizer" \
+            --model_args "pretrained=/mnt/blob-openpai-xiaoliuinterns-out/${file_name}/ckpt-globalstep${step}/,tokenizer=/mnt/blob-openpai-xiaoliuinterns-out/hongyi_he/Llama-3-8B-tokenizer" \
             --tasks arc_easy,arc_challenge,hellaswag,sciq,piqa \
             --device all \
             --output_path /mnt/blob-openpai-xiaoliuinterns-out/${file_name}/ckpt-globalstep${step}/eval_results_rebuttal.json \
